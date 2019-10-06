5cdfc07370e5e709298c4e312ff427ba2c8bb3ef
Mike Nagle <me@mikenagle.me>
2019-10-06 09:13:23 +0100

    smut

---
+++ smut_questions.js
@@ -0,0 +1,201 @@
+var gameType = "smut";
+
+var questions = [
+    {
+        id: 1,
+        question: 'What’s the dirtiest text you’ve ever sent or received'
+    },   
+    {
+        id: 2,
+        question: 'Describe a fantasy you have',
+    },
+    {
+        id: 3,
+        question: 'Deos sex have to be part of a loving relationship',
+    },
+    {
+        id: 4,
+        question: 'Would you like to be touched and held more',
+    },
+    {
+        id: 5,
+        question: 'What would an instruction manual for yourself in bed say',
+    },
+    {
+        id: 6,
+        question: 'Describe a objects that sexually interests you...a whip, a uniform, food...',
+    },
+   {
+        id: 7,
+        question: 'Do you like to be in control',
+    },
+    {
+        id: 8,
+        question: 'Do you like to be tied up',
+    },
+    {
+        id: 9,
+        question: 'Do you like to tie your partner up',
+    },
+    {
+        id: 10,
+        question: 'Is there anything you won’t do in bed',
+    },
+    {
+        id: 11,
+        question: 'How often do you want sex',
+    },
+    {
+        id: 12,
+        question: 'Would you consider a threesome',
+    },
+    {
+        id: 13,
+        question: 'What kind of underwear is sexy',
+    },
+    {
+        id: 14,
+        question: 'What is the most sensitive part of your body',
+    },
+    {
+        id: 15,
+        question: 'When was the last time you masterbated',
+    },
+        {
+        id: 16,
+        question: 'Hair or bare',
+    }, 
+        {
+        id: 17,
+        question: 'What do you sleep in',
+    },  
+        {
+        id: 18,
+        question: 'Which celebrity would you like to sleep with',
+    },  
+        {
+        id: 20,
+        question: 'How vocal are you',
+    },  
+    {
+        id: 21,
+        question: 'Have you ever been to a strip club',
+    },  
+    {
+        id: 22,
+        question: 'Have you ever had anal sex',
+    },  
+    {
+        id: 23,
+        question: 'Do you like to talk dirty',
+    },  
+    {
+        id: 24,
+        question: 'When is the best time of day to have sex',
+    },  
+    {
+        id: 25,
+        question: 'What\'s the strangest place you have ever masturbated',
+    },  
+    {
+        id: 26,
+        question: 'What is the stragest prop you have ever used to get yourself off',
+    },  
+    {
+        id: 27,
+        question: 'Have you ever had sex with someone you can\'t remember the name of',
+    },  
+    {
+        id: 28,
+        question: 'What is your favourite thing about a quickie',
+    },  
+    {
+        id: 29,
+        question: 'Have you ever been with anyone of the same sex',
+    },  
+    {
+        id: 30,
+        question: 'Do you watch porn? What kind',
+    },  
+    {
+        id: 31,
+        question: 'What is the most flattering thing someone\'s said about your naked body',
+    },  
+    {
+        id: 32,
+        question: 'What\s the most embarassing thing that has ever happened to you during sex',
+    },  
+    {
+        id: 33,
+        question: 'Have you ever sent nudes of yourself',
+    },  
+    {
+        id: 34,
+        question: 'Do you like touching yourself in front of the people you sleep with',
+    },  
+    {
+        id: 35,
+        question: 'Leather or lace',
+    },
+    {
+        id: 36,
+        question: 'Do you like spanking',
+    },  
+    {
+        id: 37,
+        question: 'What is your favourite sex toy you have ever used',
+    },  
+    {
+        id: 38,
+        question: 'What one thing you would like to buy from a sex shop but never have',
+    },  
+    {
+        id: 39,
+        question: 'What makes good sex',
+    },
+    {
+        id: 40,
+        question: 'Bum, boobs or legs',
+    },
+    {
+        id: 41,
+        question: 'What is the most unusual place you have had sex',
+    },
+//    {
+//        id: 42,
+//        question: '',
+//    },
+//    {
+//        id: 43,
+//        question: '',
+//    },
+//    {
+//        id: 44,
+//        question: '',
+//    },
+//    {
+//        id: 45,
+//        question: '',
+//    },
+//    {
+//        id: 46,
+//        question: '',
+//    },
+//    {
+//        id: 47,
+//        question: '',
+//    },
+//    {
+//        id: 48,
+//        question: '',
+//    },
+//    {
+//        id: 49,
+//        question: '',
+//    },
+//    {
+//        id: 50,
+//        question: '',
+//    },
+//    
+]
